de4c3602639ec1ef54de9d060100aee62b80faf8
mikelear <mikelear@users.noreply.github.com>
2026-05-11 13:52:42 +0000

    chore(deps): upgrade leartech_qa_canary package -> 0.0.7

--- leartech_qa_canary/configuration.py
+++ leartech_qa_canary/configuration.py
@@ -527,7 +527,7 @@ conf = leartech_qa_canary.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 0.0.1\n"\
-               "SDK Package Version: 0.0.6".\
+               "SDK Package Version: 0.0.7".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self) -> List[HostSetting]:
