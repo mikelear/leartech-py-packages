b715b6296fff7c546eac3964388bc2c201a55c54
mikelear <mikelear@users.noreply.github.com>
2026-07-29 15:40:02 +0000

    chore(deps): upgrade leartech_ai_gateway package -> 0.0.38

--- leartech_ai_gateway/configuration.py
+++ leartech_ai_gateway/configuration.py
@@ -497,7 +497,7 @@ class Configuration:
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 0.1.0\n"\
-               "SDK Package Version: 0.0.37".\
+               "SDK Package Version: 0.0.38".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self) -> List[HostSetting]:
