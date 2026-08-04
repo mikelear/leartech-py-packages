1bb25cf6b019a0c712533b834f90b2d1122cd392
mikelear <mikelear@users.noreply.github.com>
2026-08-04 14:14:49 +0000

    chore(deps): upgrade leartech_plan_conformance_consumer package -> 0.0.1

---
+++ leartech_plan_conformance_consumer/api/__init__.py
@@ -0,0 +1,22 @@
+# flake8: noqa
+
+if __import__("typing").TYPE_CHECKING:
+    # import apis into api package
+    from leartech_plan_conformance_consumer.api.example_api import ExampleApi
+    from leartech_plan_conformance_consumer.api.health_api import HealthApi
+    
+else:
+    from lazy_imports import LazyModule, as_package, load
+
+    load(
+        LazyModule(
+            *as_package(__file__),
+            """# import apis into api package
+from leartech_plan_conformance_consumer.api.example_api import ExampleApi
+from leartech_plan_conformance_consumer.api.health_api import HealthApi
+
+""",
+            name=__name__,
+            doc=__doc__,
+        )
+    )
